20ee9cf2f1cd3e80812ccfb8341fbb1d3fa2e983
jamison904 <james.jamison85@gmail.com>
2013-04-13 11:08:42 -0400

    initial commit

---
+++ n80xx-common.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+-include device/samsung/smdk4412-common/common.mk
+DEVICE_PACKAGE_OVERLAYS += device/samsung/n80xx-common/overlay-common
+
+PRODUCT_AAPT_CONFIG := xlarge mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 1280
+
+# Init files
+PRODUCT_COPY_FILES += \
+    device/samsung/n80xx-common/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc \
+    device/samsung/n80xx-common/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc
+
+# Packages
+PRODUCT_PACKAGES += \
+    libsecril-client \
+    GalaxyNoteTabSettings \
+    TVOutDummy
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+
+$(call inherit-product-if-exists, vendor/samsung/n80xx/n80xx-vendor.mk)
