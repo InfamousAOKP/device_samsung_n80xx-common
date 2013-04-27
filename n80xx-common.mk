29cc8c32999d8f435273f2673d16305b29aeb9bd
jamison904 <james.jamison85@gmail.com>
2013-04-26 20:45:44 -0400

    rebase

--- n80xx-common.mk
+++ n80xx-common.mk
@@ -17,7 +17,7 @@
 -include device/samsung/smdk4412-common/common.mk
 DEVICE_PACKAGE_OVERLAYS += device/samsung/n80xx-common/overlay-common
 
-PRODUCT_AAPT_CONFIG := xlarge mdpi hdpi
+PRODUCT_AAPT_CONFIG := xlarge mdpi
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 
 TARGET_SCREEN_HEIGHT := 800
@@ -26,7 +26,8 @@ TARGET_SCREEN_WIDTH := 1280
 # Init files
 PRODUCT_COPY_FILES += \
     device/samsung/n80xx-common/ueventd.smdk4x12.rc:root/ueventd.smdk4x12.rc \
-    device/samsung/n80xx-common/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc
+    device/samsung/n80xx-common/ueventd.smdk4x12.rc:recovery/root/ueventd.smdk4x12.rc \
+    device/samsung/n80xx-common/fstab.smdk4x12:root/fstab.smdk4x12
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -41,5 +42,10 @@ PRODUCT_PACKAGES += \
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+
+# Set product characteristic to tablet, needed for some ui elements
+PRODUCT_CHARACTERISTICS := tablet
+
+$(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/n80xx/n80xx-vendor.mk)
